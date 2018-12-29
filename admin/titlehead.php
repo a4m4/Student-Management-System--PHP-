ef221377ec4982f07ba7b871813b326a21e68ec3
Ali Mehdi <salimehdi144@gmail.com>
2018-12-30 00:56:49 +0500

    Create titlehead.php

---
+++ admin/titlehead.php
@@ -0,0 +1,10 @@
+
+<a href="admindash.php" class="back">Back To Dashboard</a>
+<a href="logout.php" class="logout">Logout</a>
+			<div class="dashtitle" align="center">
+
+				<h1>Welcome To Admin Dashboard</h1>
+
+			</div>
+
+			<br><br><br> 
